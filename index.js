351fc6724c5d7fb27c2935ca0d54f8175a34f5ed
Molly <mojonesycreative@gmail.com>
2022-09-30 22:24:45 -0400

    inner text message upon submit

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-/* modal popup */
 const modal = document.getElementById("modal");
+const modalText = document.getElementById("modal-text");
+const closeModalButton = document.getElementById("close-modal-btn");
+const consentForm = document.getElementById("consent-form");
 
+
+/* modal popup */
 setTimeout(() => {
     modal.style.display = "inline";
 }, 3500);
 
 
 /* modal close btn */
-const closeModalButton = document.getElementById("close-modal-btn");
-
 closeModalButton.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
 
 /* form submit */
-const consentForm = document.getElementById("consent-form");
-
 consentForm.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("form submitted");
+
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="upload-text">
+                Uploading your data to the dark-ish web...
+            </p>
+        </div>`
+
+    setTimeout(() => {
+        document.getElementById("upload-text").innerText = "Completing transaction...";
+    }, 2000);
 });
