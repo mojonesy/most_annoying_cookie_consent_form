e85eb3c1441208231143bab06b854a8d6689ce34
Molly <mojonesycreative@gmail.com>
2022-09-30 20:16:16 -0400

    submit form prevent default

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ const closeModalButton = document.getElementById("close-modal-btn");
 closeModalButton.addEventListener("click", () => {
     modal.style.display = "none";
 });
+
+
+/* form submit */
+const consentForm = document.getElementById("consent-form");
+
+consentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+});
